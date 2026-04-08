1cfbdc6ca52d33e06127de802d02beb4e8fa0a9a
Kyle Bradshaw <itllbok82@gmail.com>
2026-04-07 23:38:00 -0600

    refactor(ecommerce): rename migrations and split seed

---
+++ go/ecommerce-service/migrations/001_create_products.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_products_price;
+DROP INDEX IF EXISTS idx_products_category;
+DROP TABLE IF EXISTS products;
---
+++ go/ecommerce-service/migrations/003_create_orders.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_order_items_order;
+DROP TABLE IF EXISTS order_items;
+DROP INDEX IF EXISTS idx_orders_status;
+DROP INDEX IF EXISTS idx_orders_user;
+DROP TABLE IF EXISTS orders;
